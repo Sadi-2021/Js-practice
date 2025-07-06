a22f8a8383c436e0e20dce263aa3ee6bdc2b5137
Md. Sameul Alime Sadi <shamiul1999@gmail.com>
2025-07-05 18:17:14 -0700

    file move and edit

---
+++ Operators/app.js
@@ -0,0 +1,23 @@
+// Additional Operator ( + )
+let fName = "Samiul "
+let lName = "Alime"
+
+console.log(fName + lName)
+
+// Subtraction ( - )
+
+let x = 5;
+let y = 3;
+
+console.log('this is Subtraction =', x-y)
+// Multiplication ( * )
+
+console.log( 'this is multiplication =', x * y)
+
+// Division ( / )
+
+console.log('this is a division =', x / y)
+
+// Modules ( % )
+
+console.log( 'this is a Modules of =', x % y)
